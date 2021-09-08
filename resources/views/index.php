9d1088b0c1908e31db1e483154f55e6adb48d0ff
Pelle Boese <boese@forthewin.de>
2021-09-08 09:14:48 +0200

    all groups in one table

--- resources/views/index.php
+++ resources/views/index.php
@@ -215,47 +215,51 @@ $deleteEnabled = false;
 
         <?php else: ?>
 
-            <?php foreach($allTranslationsByGroup as $group => $groupTranslations): ?>
-                <?php if($groupTranslations && isset($groupTranslations['translations']) && $group != '_json' && $group != 'vendor/authentication-log'): ?>
-                    <hr>
-                    <h3><?= $group ?></h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th width="15%">Key</th>
+                    <?php foreach ($locales as $locale): ?>
+                        <th><?= $locale ?></th>
+                    <?php endforeach; ?>
+                </tr>
+                </thead>
+                <tbody>
 
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th width="15%">Key</th>
-                            <?php foreach ($locales as $locale): ?>
-                                <th><?= $locale ?></th>
-                            <?php endforeach; ?>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        <?php foreach ($groupTranslations['translations'] as $key => $translation): ?>
-                            <tr id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
-                                <td><?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?></td>
-                                <?php foreach ($locales as $locale): ?>
-                                    <?php $t = isset($translation[$locale]) ? $translation[$locale] : null ?>
-
-                                    <td>
-                                        <a href="#edit"
-                                           class="editable status-<?php echo $t ? $t->status : 0 ?> locale-<?php echo $locale ?>"
-                                           data-locale="<?php echo $locale ?>"
-                                           data-name="<?php echo $locale . "|" . htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>"
-                                           id="username"
-                                           data-type="textarea"
-                                           data-pk="<?php echo $t ? $t->id : 0 ?>"
-                                           data-url="<?php echo $groupTranslations['editUrl'] ?>"
-                                           data-title="Enter translation"><?php echo $t ? htmlentities($t->value, ENT_QUOTES, 'UTF-8', false) : '' ?></a>
-                                    </td>
-                                <?php endforeach; ?>
+                    <?php foreach($allTranslationsByGroup as $group => $groupTranslations): ?>
+                        <?php if($groupTranslations && isset($groupTranslations['translations']) && $group != '_json' && $group != 'vendor/authentication-log'): ?>
+                            <tr>
+                                <td colspan="<?= count($locales) + 1 ?>">
+                                    <h3><?= $group ?></h3>
+                                </td>
                             </tr>
-                        <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                <?php endif ?>
 
-            <?php endforeach ?>
+                                <?php foreach ($groupTranslations['translations'] as $key => $translation): ?>
+                                    <tr id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
+                                        <td><?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?></td>
+                                        <?php foreach ($locales as $locale): ?>
+                                            <?php $t = isset($translation[$locale]) ? $translation[$locale] : null ?>
+
+                                            <td>
+                                                <a href="#edit"
+                                                   class="editable status-<?php echo $t ? $t->status : 0 ?> locale-<?php echo $locale ?>"
+                                                   data-locale="<?php echo $locale ?>"
+                                                   data-name="<?php echo $locale . "|" . htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>"
+                                                   id="username"
+                                                   data-type="textarea"
+                                                   data-pk="<?php echo $t ? $t->id : 0 ?>"
+                                                   data-url="<?php echo $groupTranslations['editUrl'] ?>"
+                                                   data-title="Enter translation"><?php echo $t ? htmlentities($t->value, ENT_QUOTES, 'UTF-8', false) : '' ?></a>
+                                            </td>
+                                        <?php endforeach; ?>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php endif ?>
+                    <?php endforeach ?>
+
+                </tbody>
+            </table>
+
 
         <?php endif; ?>
 
