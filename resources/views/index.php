ef0c628a2f89ed3f4cfe1267df039096c79b8f0d
Pelle Boese <boese@forthewin.de>
2021-09-08 09:58:55 +0200

    display missing translations only option

--- resources/views/index.php
+++ resources/views/index.php
@@ -153,6 +153,12 @@ $deleteEnabled = false;
         </form>
         <?php endif; ?>
 
+        <div class="checkbox">
+            <label>
+                <input value="1" type="checkbox" id="toggle_display_missing_only" checked> <b>Display missing translations only</b>
+            </label>
+        </div>
+
         <?php if(isset($group)) : ?>
             <a href="<?= action('\Barryvdh\TranslationManager\Controller@getIndex') ?>" class="btn btn-default">Back</a>
         <?php endif; ?>
@@ -163,7 +169,6 @@ $deleteEnabled = false;
         <form role="form" method="POST" action="<?php echo action('\Barryvdh\TranslationManager\Controller@postAddGroup') ?>">
             <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
             <div class="form-group">
-                <p>Choose a group to display. If no groups are visisble, make sure you have imported the translations.</p>
                 <select name="group" id="group" class="form-control group-select">
                     <?php foreach($groups as $key => $value): ?>
                         <?php if($key != '_json' && $key != 'vendor/authentication-log'): ?>
@@ -235,7 +240,9 @@ $deleteEnabled = false;
                             </tr>
 
                                 <?php foreach ($groupTranslations['translations'] as $key => $translation): ?>
-                                    <tr id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
+                                    <tr
+                                        data-has-empty=""
+                                        id="<?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?>">
                                         <td><?php echo htmlentities($key, ENT_QUOTES, 'UTF-8', false) ?></td>
                                         <?php foreach ($locales as $locale): ?>
                                             <?php $t = isset($translation[$locale]) ? $translation[$locale] : null ?>
@@ -266,6 +273,32 @@ $deleteEnabled = false;
     </div>
 
 </div>
+
+<script>
+    function onlyEmpty() {
+        $.each($('tr:has(a.editable)'), function() {
+            if($(this).find('a.editable-empty').length == 0) {
+                $(this).hide()
+            }
+        })
+    }
+
+    function showAll() {
+        $('tr').show()
+    }
+
+    $(function() {
+        onlyEmpty()
+
+        $('#toggle_display_missing_only').click(function() {
+            if($(this).prop('checked')) {
+                onlyEmpty()
+            } else {
+                showAll()
+            }
+        })
+    })
+</script>
 
 </body>
 </html>
